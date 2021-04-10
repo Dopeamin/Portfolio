f13d7de1090d3d0930eb83e220c9a8cd68ef0a5f
Med Amine Hamdouni <mohamedaminehamdouni@insat.u-carthage.tn>
2021-04-10 11:09:23 +0100

    started protfolio

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Med Amine Hamdouni</h1>
+        <p>Full Stack Developer</p>
       </header>
+      <div className="App-section">
+        <div className="container">
+          <div className="title">
+            <h3>Explore, Create, Innovate</h3>
+            <h1>Full Stack<br></br>Web Developer</h1>
+            <p>My name is Mohamed Amine Hamdouni, Hard working Software Engineering Student with problem leadership, organizational and communcation skills 
+              seeking to apply my skills. I'm curious by nature as im always intrigued about new technologies and how to solve problems differently and effectively
+            </p>
+            <div className="button">
+              <h3>Contact me</h3>
+            </div>
+          </div>
+          
+          <img src="assets/section1.svg" width="60%"></img>
+        </div>
+        
+      </div>
     </div>
   );
 }
